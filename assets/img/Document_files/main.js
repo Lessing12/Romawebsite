1572a994ce8a271e72c16c47887db03b9a91c7f9
Marcin <mgabrys422@gmail.com>
2020-02-12 14:38:06 +0100

    text2

---
+++ assets/img/Document_files/main.js
@@ -0,0 +1,34 @@
+let days = 7;
+let dayweek = 5;
+let oneTime=1;
+let twoTime=2;
+let threeTime=3;
+
+
+console.log(days);
+console.log("Day")
+console.log('day')
+console.log('DaY')
+console.log("Take this medicament 3 times a day in " + days + " days");
+console.log("take the medicine 3 times a day in " + dayweek + " days");
+console.log("take the medicament " + twoTime + " times a day in ö" + days + " days")
+document.write("Some text more");
+document.write("<hi> I am super Big </h>")
+let a = 1;
+let b = 2;
+let c = 1+2;
+console.log("c is equal to " + c);
+
+let hiinger = " hallo";
+let hiinspan = " hola";
+let hiinfrench = "salut";
+let sayhi;
+sayhi = hiinfrench + hiinger + hiinspan;
+console.log(sayhi)
+
+
+let firstName = "Marcin";
+let secondName = "Gab";
+let gretting;
+gretting = "danke " + firstName + " "+secondName + " die Bestellung";
+console.log(gretting)
